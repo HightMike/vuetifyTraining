b0b062a1b28a58e4da35eb627e002221787e3ebd
hightmike <baobab-borisov@mail.ru>
2020-02-26 23:39:23 +0300

    play with some styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <h1 class="display-4">Big text</h1>
+
+    <v-btn class="pink">Кнопка</v-btn>
+    <v-btn depressed large color="primary">Другая</v-btn>
+    <v-btn class="pink v-btn--flat">Еще одна</v-btn>
+    <v-btn small color="purple">
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+    <v-btn class="hidden-md-and-down">Исчезающая</v-btn>
+
   </div>
 </template>
 
